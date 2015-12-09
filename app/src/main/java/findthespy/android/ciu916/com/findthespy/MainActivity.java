fc8da144eeedbd6a02268741ff0a101a9018fa76
Zhenyu Zhang <Zhenyu@Zhenyus-MacBook-Pro.local>
2015-12-09 15:04:56 +0100

    Initial commit

---
+++ app/src/main/java/findthespy/android/ciu916/com/findthespy/MainActivity.java
@@ -0,0 +1,13 @@
+package findthespy.android.ciu916.com.findthespy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
